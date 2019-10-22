15a9f332a4f82a526b1561c417bce0da1e2cb526
Agnese Bartolucci <agnesebartolucci505@gmail.com>
2019-10-23 01:39:01 +0200

    Assignement_05

--- sketch.js
+++ sketch.js
@@ -1,11 +1,46 @@
+var mic;
+var x = 10;
+var y = 15;
+var colorList = ['#fe4365', '#fc9d9a', '#f9cdad', '#c8c8a9', '#83af9b'];
+
 function preload(){
-  // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+  background(0, 20);
+
+  textFont("Roboto Mono");
+  textAlign (LEFT)
+  textSize(20);
+  fill(255);
+  noStroke();
+  var myText1 = "Use keyboard arrow keys and make noise";
+  text(myText1, width/2 - 250, height/2);
+
+  var vol = mic.getLevel();
+
+  if (keyIsDown(LEFT_ARROW)) {
+      x -= 5 * random();
+    }
+  if (keyIsDown(RIGHT_ARROW)) {
+      x += 5 * random();
+    }
+
+  if (keyIsDown(UP_ARROW)) {
+      y -= 5 * random();
+    }
+
+  if (keyIsDown(DOWN_ARROW)) {
+      y += 5 * random();
+    }
+
+  noStroke();
+  fill(color(random(colorList)));
+  ellipse(x, y, vol * 50, vol * 50);
 }
